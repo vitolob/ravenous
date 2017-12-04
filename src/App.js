4059d56aef5bf5c310ccbc22cf3b3443e917f0dd
Victorino Machava <vitomachava@gmail.com>
2017-12-04 12:58:36 +0200

    Create businesses array in App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [];
+
+for (let i = 0; i < 6; i++) {
+  businesses.push(business);
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
